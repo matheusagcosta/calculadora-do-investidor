ad62694a8e059bf27424187795fd50752a96db68
guicos <gui.coxta55@gmail.com>
2021-04-05 22:59:49 -0300

    add title, header and input section (html)

--- code.js
+++ code.js
@@ -1,14 +1,12 @@
-avgPrice = 0
+var avgPrice = 0
+var tot = 0
+
 
 function calcPrice(qnt=Array, price=Array) {
-    
+
     for (i= 0; i< qnt.length; i++) {
-      avgPrice += qnt[i]*price[i]
+      tot += qnt[i]*price[i]
     }
-    document.write(" " + avgPrice)
-    avgPrice /= qnt.length
-    return avgPrice
+    avgPrice = tot/qnt.length
+    return tot, avgPrice
 }
-document.write(" " + calcPrice([500,200,100], [10,10,10]).toFixed(2))
-
-
