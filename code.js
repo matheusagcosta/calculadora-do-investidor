6263c6bb38b4567eb08444113808dc4909a43a38
guicos <gui.coxta55@gmail.com>
2021-04-05 21:10:21 -0300

    base js calc structure

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
-function calcPrice(qnt=Array, avgPric=Array) {
-  return document.write('aaaaa')
+avgPrice = 0
+
+function calcPrice(qnt=Array, price=Array) {
+    
+    for (i= 0; i< qnt.length; i++) {
+      avgPrice += qnt[i]*price[i]
+    }
+    document.write(" " + avgPrice)
+    avgPrice /= qnt.length
+    return avgPrice
 }
-
-var quant = []
-var average = []
-
-
-calcPrice(quant, average)
+document.write(" " + calcPrice([500,200,100], [10,10,10]).toFixed(2))
 
 
