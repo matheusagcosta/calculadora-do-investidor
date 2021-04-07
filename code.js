688d7a7d03d892faefe8a07b933b5d6d7249cf16
guicos <gui.coxta55@gmail.com>
2021-04-06 22:10:17 -0300

    try to display multiple tables

--- code.js
+++ code.js
@@ -2,10 +2,15 @@ var avgPrice = 0
 var tot = 0
 
 function calcPrice(qnt=Array, price=Array) {
-
+    html = ""
     for (i= 0; i< qnt.length; i++) {
       tot += qnt[i]*price[i]
     }
     avgPrice = tot/qnt.length
+
+    html += "<table align='center'><tr><td>Quantidade:</td><td>Preço Médio:</td></tr></table>"
+    content_table = document.getElementById("data")
+    content_table.innerHTML = html
+
     return tot, avgPrice
 }
