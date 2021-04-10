59824ad75a147692545077cd4dd6eb2233f26828
guicos <gui.coxta55@gmail.com>
2021-04-10 15:05:18 -0300

    add svg icons to header + js and html rework

--- code.js
+++ code.js
@@ -1,12 +1,14 @@
 var avgPrice = 0
 var tot = 0
-count = 0
+var count = 0
 var arrValues = []
 var arrTemp = []
 
 function calcPrice() {
-  html = `
-  <div class="data" id="data${count+1}" style="visibility: visible;">
+  count ++
+
+  var html = `
+  <div class="data" id="data${count}" style="visibility: visible;">
       <table align="center" > 
         <tr>
           <td id="quantidade">Quantidade:</td>
@@ -14,39 +16,42 @@ function calcPrice() {
         </tr>
         <tr>
           <td>
-            <label><input type="number" id="quantityN${count+1}" min=0 value=0>
+            <label><input type="number" id="quantityN${count}" min="0" value="">
             </label>
           </td>
           <td>
             <span>R$</span>
-            <label><input type="number" id="priceN${count+1}" min=0 value=0.00>
+            <label><input type="number" id="priceN${count}" min="0" value="">
             </label>
           </td>
           <td>
-            <button class="trash" id="trash${count+1}" onClick="wipeOut(${count+1})" style="visibility: visible;" >🔥</button>
+            <button class="trash" id="trash${count}" onClick="wipeOut(${count})" style="visibility: visible;" >🔥</button>
           </td>
         </tr>
       </table> 
   </div>
   `;
+ 
   if (count == 0) {
-    document.getElementById("trash0").style.visibility = "visible";
-    document.getElementById("data0").innerHTML += html
+    document.getElementById(`trash${count}`).style.visibility = "hidden";
   } else {
-    document.getElementById(`data${count}`).innerHTML += html
+    document.getElementById(`trash${count-1}`).style.visibility = "visible";
   }
-  /*
+  document.getElementById("section").innerHTML += html
+  
+  
   quant = document.getElementById(`quantityN${count}`).value
   prc = document.getElementById(`priceN${count}`).value
-
   if (quant!=0 && prc!=0.00) {
     arrTemp.push(document.getElementById(`quantityN${count}`).value)
     arrTemp.push(document.getElementById(`priceN${count}`).value)
     arrValues.push(arrTemp)
   }
-  */
+  
+  tot = quant
+  avgPrice
   attFooter()
-  count ++ 
+   
 }
 
 function attFooter() {
