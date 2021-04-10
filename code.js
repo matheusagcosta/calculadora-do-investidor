66421df580312994d57b22029f079a98fec132ac
guicos <gui.coxta55@gmail.com>
2021-04-10 00:51:21 -0300

    background to white

--- code.js
+++ code.js
@@ -1,35 +1,34 @@
 var avgPrice = 0
 var tot = 0
 count = 0
+var arrValues = []
+var arrTemp = []
 
 function calcPrice() {
-      
   html = `
-  
-    
-      <div class="data" id="data${count+1}" style="visibility: visible;">
-          <table align="center" > 
-            <tr>
-              <td id="quantidade">Quantidade:</td>
-              <td id="preco">Preço Médio:</td>
-            </tr>
-            <tr>
-              <td>
-                <label><input type="number" id="quantityN${count+1}" min=0></label>
-              </td>
-              <td>
-                <span>R$</span>
-                <label><input type="number" id="priceN${count+1}" min=0></label>
-              </td>
-              <td id="trash${count+1}">
-                <button class="trash" onClick="wipeOut()" style="visibility: visible;" >🔥</button>
-              </td>
-            </tr>
-          </table> 
-      </div>
-    
+  <div class="data" id="data${count+1}" style="visibility: visible;">
+      <table align="center" > 
+        <tr>
+          <td id="quantidade">Quantidade:</td>
+          <td id="preco">Preço Médio:</td>
+        </tr>
+        <tr>
+          <td>
+            <label><input type="number" id="quantityN${count+1}" min=0 value=0>
+            </label>
+          </td>
+          <td>
+            <span>R$</span>
+            <label><input type="number" id="priceN${count+1}" min=0 value=0.00>
+            </label>
+          </td>
+          <td id="trash${count+1}">
+            <button class="trash" onClick="wipeOut()" style="visibility: visible;" >🔥</button>
+          </td>
+        </tr>
+      </table> 
+  </div>
   `;
-  
   if (count == 0) {
     document.getElementById("trash0").style.visibility = "visible";
     document.getElementById("data0").innerHTML += html
@@ -37,24 +36,24 @@ function calcPrice() {
     document.getElementById(`data${count}`).innerHTML += html
   }
 
-  
+  //quantidade = `quantityN${count}`
+  //tot = document.getElementById(quantidade).value;
   
   attFooter()
   count ++ 
 }
 
 function attFooter() {
-  var changeFooter = document.getElementById("footer")
-
-  var html2 = `
+  
+  html2 = `
   <label> Quantidade Total: &nbsp;&nbsp; Preço Médio: </label> <br>
   <span class="quantidade">${tot}</span>
   <span class="preco"> R$ ${avgPrice}</span>
   `;
-  
+  changeFooter = document.getElementById("footer")
   changeFooter.innerHTML = html2
 }
 
 function wipeOut() {
-    document.getElementById(this).remove()
+    //document.getElementById(this).remove()
 }
