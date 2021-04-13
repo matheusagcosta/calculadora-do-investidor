ef441a497df5c4d78e3c7242362adea72701bbee
guicos <gui.coxta55@gmail.com>
2021-04-12 21:05:16 -0300

    change 'count' logic for 'x variable' logic

--- code.js
+++ code.js
@@ -1,18 +1,16 @@
 var avgPrice = 0
 var tot = 0
-var count = 0
 var arrValues = []
 var arrTemp = []
-var quant
-var price
 var verifQ
 var verifP
+var x = 0
 
 function calcPrice() {
-  count ++
+  x ++
 
-  verifQ = document.getElementById(`quantityN${count-1}`).value 
-  verifP = document.getElementById(`priceN${count-1}`).value
+  verifQ = document.getElementById(`quantityN${x-1}`).value 
+  verifP = document.getElementById(`priceN${x-1}`).value
 
   if (!(parseFloat(verifQ))) {
     verifQ = 0
@@ -32,10 +30,13 @@ function calcPrice() {
   arrValues.push(arrTemp)
   arrTemp=[]
   
+  for (u=1; u < arrValues.length; u++) {
+
+  }
+
   
-  quant = price = "0"
   var html = `
-  <div class="data" id="data${count}" style="visibility: visible;">
+  <div class="data" id="data${x}" style="visibility: visible;">
       <table align="center" > 
         <tr>
           <td id="quantidade">Quantidade:</td>
@@ -43,39 +44,34 @@ function calcPrice() {
         </tr>
         <tr>
           <td>
-            <label><input type="text" id="quantityN${count}" min="0" value="${quant}">
+            <label><input type="text" id="quantityN${x}" min="0" value="">
             </label>
           </td>
           <td>
             <span>R$</span>
-            <label><input type="text" id="priceN${count}" min="0" value="${price}">
+            <label><input type="text" id="priceN${x}" min="0" value="">
             </label>
           </td>
           <td>
-            <button class="trash" id="trash${count}" onClick="wipeOut(${count})" style="visibility: visible;" >🔥</button>
+            <button class="trash" id="trash${x}" onClick="wipeOut(${x})" style="visibility: visible;" >🔥</button>
           </td>
         </tr>
       </table> 
   </div>
   `;
-  
-  for (i=0; i < count; i++) {
-    if (arrValues[i][0] != -1 && arrValues[i][1] != -1) {
-      document.getElementById(`quantityN${i}`).value = toString(arrValues[i][0])
-      document.getElementById(`priceN${i}`).value = toString(arrValues[i][1])
-    }
+
+  if (arrValues.length == 0) {
+    document.getElementById(`trash0`).style.visibility = "hidden";
   }
-
-
-
-  if (count == 0) {
-    document.getElementById(`trash${count}`).style.visibility = "hidden";
+  if (x==1 && arrValues.length == 1) {
+    document.getElementById(`trash0`).style.visibility = "hidden";
   } else {
-    document.getElementById(`trash${count-1}`).style.visibility = "visible";
-  }
+    document.getElementById(`trash0`).style.visibility = "visible";
+  } 
+  
   document.getElementById("section").innerHTML += html
   
-  tot=arrValues.length
+  
   attFooter()
    
 }
@@ -91,8 +87,24 @@ function attFooter() {
   changeFooter.innerHTML = html2
 }
 
-function wipeOut(x) {
-    document.getElementById(`data${x}`).remove()
-    arrValues[x][0] = -1
-    arrValues[x][1] = -1
+function wipeOut(y) {
+    
+  if (y==x) {
+    document.getElementById(`data${y}`).remove()
+  } else {
+    document.getElementById(`data${y}`).remove()
+    arrValues[y][0] = -1
+    arrValues[y][1] = -1
+  }
+  c = 0
+  t = 0
+  for (i=0;i<arrValues.length;i++) {
+    if (arrValues[i][0]!=-1) {
+      t = i
+      c += 1
+    }
+  }
+  if (c==1) {
+    document.getElementById(`trash${t}`).style.visibility = "hidden";
+  }
 }
