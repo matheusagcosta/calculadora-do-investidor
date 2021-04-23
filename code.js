7385feddf60a4527fc9988f6c23db605fc904912
guicos <gui.coxta55@gmail.com>
2021-04-22 21:42:33 -0300

    wipe 'data' stuff out of js

--- code.js
+++ code.js
@@ -8,83 +8,39 @@ var verifP
 var x = 0
 
 function calcPrice() {
-  x ++
-  // input validation system 
-  verifQ = document.getElementById(`quantityN${x-1}`).value 
-  verifP = document.getElementById(`priceN${x-1}`).value
-
-  if (verifQ == "" || verifP == "") {
-    
-    x--
-    
-  } else {
-    
-    // value validation
-    if (!(parseFloat(verifQ))) {
-      verifQ = 0
-    }
-    if (!(parseFloat(verifP))) {
-      verifP = 0
-      verifQ = 0
-    }
-    if (verifQ<=0) {
-      verifQ = 0
-    }
-    if (verifP<=0) {
-      verifP = 0
-      verifQ = 0
-    }
-    
-    // array addition
-    verifQ = parseFloat(verifQ)
-    verifP = parseFloat(verifP)
-    arrTemp.push(verifQ)
-    arrTemp.push(verifP)
-    arrValues.push(arrTemp)
-    arrTemp=[]
-
-
-    // data creation
-    var html = `
-    <div class="data" id="data${x}" style="visibility: visible;">
-        <table align="center" > 
-          <tr>
-            <td id="quantidade">Quantidade:</td>
-            <td id="preco">Preço Médio:</td>
-          </tr>
-          <tr>
-            <td>
-              <label><input type="text" id="quantityN${x}" min="0" value="" required>
-              </label>
-            </td>
-            <td>
-              <span>R$</span>
-              <label><input type="text" id="priceN${x}" min="0" value="" required>
-              </label>
-            </td>
-            <td>
-              <button class="trash" id="trash${x}" onclick="wipeOut(${x})" style="visibility: visible;">🔥</button>
-            </td>
-          </tr>
-        </table> 
-    </div>
-    `;
-    document.getElementById("section").innerHTML += html
-
-    // calculations
-    tot += verifQ
-    products += verifQ*verifP
-    if (arrValues.length == 1) {
-      avgPrice = arrValues[0][1].toFixed(2)
-    } else {
-      avgPrice = (products/tot).toFixed(2)
-    }
   
-    // footer
-    attFooter()
-   
+  // input validation system 
+  verifQ = document.getElementById(`quantityN`).value 
+  verifP = document.getElementById(`priceN`).value
+    
+  // array addition
+  verifQ = parseFloat(verifQ)
+  verifP = parseFloat(verifP)
+  arrTemp.push(verifQ)
+  arrTemp.push(verifP)
+  arrValues.push(arrTemp)
+  arrTemp=[]
+
+
+  // form creation
+  var html = `
+  `;
+  document.getElementById("section").innerHTML += html
+
+  // calculations
+  tot += verifQ
+  products += verifQ*verifP
+  if (arrValues.length == 1) {
+    avgPrice = arrValues[0][1].toFixed(2)
+  } else {
+    avgPrice = (products/tot).toFixed(2)
   }
-}
+
+  // footer
+  attFooter()
+    
+  }
+
 
 
 
@@ -108,39 +64,8 @@ function attFooter() {
 }
 
 function wipeOut(y) {
-  x --
-  document.getElementById(`data${y}`).remove()
   
-  //change data id's
-    for (u=y+1; u <= arrValues.length; u++) {
-      html3 = `
-        <div class="data" id="data${u-1}" style="visibility: visible;">
-          <table align="center" > 
-            <tr>
-              <td id="quantidade">Quantidade:</td>
-              <td id="preco">Preço Médio:</td>
-            </tr>
-            <tr>
-              <td>
-                <label><input type="text" id="quantityN${u-1}" min="0" value="" required>
-                </label>
-              </td>
-              <td>
-                <span>R$</span>
-                <label><input type="text" id="priceN${u-1}" min="0" value="" required>
-                </label>
-              </td>
-              <td>
-                <button class="trash" id="trash${u-1}" onclick="wipeOut(${u-1})" style="visibility: visible;">🔥</button>
-              </td>
-            </tr>
-          </table> 
-        </div>
-      `;
-      change = document.getElementById(`data${u}`)
-      change.innerHTML = html3
-      x = arrValues.length-1
-    }
+  
 
   if (arrValues[y]) {
     tot -= arrValues[y][0]
