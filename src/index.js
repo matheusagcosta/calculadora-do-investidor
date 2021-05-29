3dc3e57a4b192f981894beda0b6fbeffb51ab207
guicos <gui.coxta55@gmail.com>
2021-05-29 11:11:12 -0300

    start parcel sync

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { addGarb, calcPrice, attFooter, wipeOut, reset } from "./js/code";
+
+document.querySelector('#addButton').addEventListener('click', calcPrice(0));
+document.querySelector('#reset').addEventListener('click', reset());
+
+
