fe6ef0e4afc92fe3cacf2b12645f53ff6b36e292
guicos <gui.coxta55@gmail.com>
2021-06-10 13:18:16 -0300

    add regExp to input validation

--- code.js
+++ code.js
@@ -11,98 +11,68 @@ const money = new Intl.NumberFormat('pt-BR', {
   minimumFractionDigits: 2
 })
 
-const addGarb = () => {
-  const garb = `
-    <form id="garb">
-    <label for="quantityN" id="quantidade">Quantidade:</label>
-    <label for="priceN" id="preco">Preço:</label>
-    <input type="text" name="quantityN" id="quantityN" min="0" value="" required>
-    <input type="text" name="priceN" id="priceN" min="0" value="" required>
-    </form>
-  `;
-  // add garb to stop the form from reloading
-  document.getElementById("section").innerHTML += garb
-  document.getElementById("garb").remove()
-  // add values to previous forms
-  for (u=0; u<document.forms.length-1; u++) {
-    document.getElementsByName("quantityN")[u].value = arrValues[u][0]
-    document.getElementsByName("priceN")[u].value = arrValues[u][1]
-  }
-}
-
 const calcPrice = (y) => {
   let arrTemp = [];
   let valueQ = "";
   let valueP = "";
-  // input validation system (1) -> empty strings
+  let re_1 = new RegExp("[0-9]+")
+  let re_2 = new RegExp("[0-9]+(,|.)?[0-9]*")
   valueQ = document.getElementsByName("quantityN")[y].value
   valueP = document.getElementsByName("priceN")[y].value
-  if (valueQ!="" && valueP!="") {
-    // input validation system (2) -> characters
-    if (valueP.replace(/,/g, '.')) {
-      valueP = valueP.replace(/,/g, '.')
-    }
-    valueQ = parseInt(valueQ)
-    valueP = parseFloat(valueP)
-    if (valueQ && valueP) {
-      // input validation system (3) -> negative numbers
-      if (valueQ>=0 && valueP>=0) {
-        // array addition
-        arrTemp.push(valueQ)
-        arrTemp.push(valueP)
-        arrValues.push(arrTemp)
-        arrTemp=[]
-        // calculations
-        tot += valueQ
-        products += valueQ*valueP
-        if (arrValues.length == 1) {
-          avgPrice = arrValues[0][1].toFixed(2)
-        } else {
-          avgPrice = (products/tot).toFixed(2)
-        }
-        // form creation
-        const html = `
-        <form class="form">
-          <div id="qForm">
-            <label for="quantityN"  class="textForm" id="quantidade">Quantidade:</label>
-            <input type="text" class="valuesForm" name="quantityN" id="quantityN" placeholder="0" min="0" value="" required>
-          </div>
-          <div id="pForm">
-            <label for="priceN" class="textForm" id="preco">Preço:</label>
-            <input type="text" class="valuesForm" name="priceN" id="priceN" placeholder="R$ 0,00" min="0" value="" required> 
-          </div>
-          <div class="trash_button" id="trash_button">
-            <button class="trash" id="trash${y+1}"></button>
-          </div>
-        </form>
-        `;
-        document.getElementById("section").innerHTML += html
-        document.getElementById("add").setAttribute("onclick", `calcPrice(${y+1})`)
-        // update trash button of previous form
-        document.getElementById(`trash${y}`).setAttribute("onclick", `wipeOut(${y})`)
-        // add values to previous forms
-        for (u=0; u<document.forms.length-1; u++) {
-          document.getElementsByName("quantityN")[u].setAttribute("value", `${arrValues[u][0]}`) 
-          document.getElementsByName("priceN")[u].setAttribute("value", `${arrValues[u][1].toFixed(2)}`)
-        }
-        // increase form size
-        if (document.forms.length == 2) {
-          document.getElementsByClassName("form")[0].setAttribute("class", "form is-bigger")
-        }
-        document.getElementsByClassName("form")[y+1].setAttribute("class", "form is-bigger")
-        // change trash button opacity
-        document.getElementsByClassName("trash_button")[y].className = "trash_button is-shown"
-        // footer
-        attFooter()
-      } else {
-          addGarb()
-      }
-    } else {
-        addGarb()
-    }
-  } else {
-      addGarb()
+  valueQ = re_1.exec(valueQ)[0]
+  valueP = re_2.exec(valueP)[0]
+  if (valueP.replace(/,/g, '.')) {
+    valueP = valueP.replace(/,/g, '.')
   }
+  valueQ = parseInt(valueQ)
+  valueP = parseFloat(valueP)
+  // array addition
+  arrTemp.push(valueQ)
+  arrTemp.push(valueP)
+  arrValues.push(arrTemp)
+  arrTemp=[]
+  // calculations
+  tot += valueQ
+  products += valueQ*valueP
+  if (arrValues.length == 1) {
+    avgPrice = arrValues[0][1].toFixed(2)
+  } else {
+    avgPrice = (products/tot).toFixed(2)
+  }
+  // form creation
+  const html = `
+  <form class="form">
+    <div id="qForm">
+      <label for="quantityN"  class="textForm" id="quantidade">Quantidade:</label>
+      <input type="text" class="valuesForm" name="quantityN" id="quantityN" placeholder="0" autocomplete="off" min="0" value="" required>
+    </div>
+    <div id="pForm">
+      <label for="priceN" class="textForm" id="preco">Preço:</label>
+      <input type="text" class="valuesForm" name="priceN" id="priceN" placeholder="R$ 0,00" autocomplete="off" min="0" value="" required> 
+    </div>
+    <div class="trash_button" id="trash_button">
+      <button class="trash" id="trash${y+1}"></button>
+    </div>
+  </form>
+  `;
+  document.getElementById("section").innerHTML += html
+  document.getElementById("add").setAttribute("onclick", `calcPrice(${y+1})`)
+  // update trash button of previous form
+  document.getElementById(`trash${y}`).setAttribute("onclick", `wipeOut(${y})`)
+  // add values to previous forms
+  for (u=0; u<document.forms.length-1; u++) {
+    document.getElementsByName("quantityN")[u].setAttribute("value", `${arrValues[u][0]}`) 
+    document.getElementsByName("priceN")[u].setAttribute("value", `${arrValues[u][1].toFixed(2)}`)
+  }
+  // increase form size
+  if (document.forms.length == 2) {
+    document.getElementsByClassName("form")[0].setAttribute("class", "form is-bigger")
+  }
+  document.getElementsByClassName("form")[y+1].setAttribute("class", "form is-bigger")
+  // change trash button opacity
+  document.getElementsByClassName("trash_button")[y].className = "trash_button is-shown"
+  // footer
+  attFooter()
 }
 
 const attFooter = () => {
