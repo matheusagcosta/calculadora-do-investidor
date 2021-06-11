7f73f9857cc0ab58a199d3ff18386e17eed1b949
guicos <gui.coxta55@gmail.com>
2021-06-11 11:06:42 -0300

    create css folder and move indx to js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import { onAddClick, attFooter, wipeOut, reset } from "./js/code";
+import { onAddClick, attFooter, wipeOut, reset } from "./code";
 
 document.querySelector("#add").addEventListener("click", onAddClick);
 //document.querySelector('#trash0').addEventListener('click', wipeOut);
